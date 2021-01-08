2b93b56026e5fb467888c3b3dd7fb1cf7f507381
Correy Everett <correy.everett@gmail.com>
2021-01-08 10:43:39 -0600

    check - is userInput a number

--- src/main/java/StringManipulator.java
+++ src/main/java/StringManipulator.java
@@ -1,8 +1,21 @@
-import static org.junit.Assert.*;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+//import static org.junit.Assert.*;
 
 public class StringManipulator {
 
     public static void  main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String message = "Please enter a word that describes you!";
+        System.out.println(message);
+        String userInput = scan.nextLine();
+
+        System.out.println(StringUtils.isNumeric(userInput));
+        System.out.println();
+
 
     }
 
